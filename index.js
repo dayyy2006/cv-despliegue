c82a86401f6dc59e910dd7601c6e56003cf2cf8b
Dayron Cayalta <dayron.cayalta@tecsup.edu.pe>
2026-05-04 12:02:24 -0500

    fix finallll

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
+
 // esto sirve TODO
 app.use(express.static(path.join(__dirname, 'public')));
 
